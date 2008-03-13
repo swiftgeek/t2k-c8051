4d90017f5549087b79cbc232da9de389096cbb65
bryerton <bryerton@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-03-13 21:58:03 +0000

    - preliminary checkin of new interrupt code
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@86 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Protocols/SMBus_handler.h
+++ Protocols/SMBus_handler.h
@@ -1,27 +1,60 @@
-// --------------------------------------------------------
-//  SMBus handling routines for T2K-FGD experiment
-//      - Writtern for T2K-ASUM test board
-//      - Brian Lee   (Jun/08/2006)
-//
-//  $Id$
-// --------------------------------------------------------
+/**********************************************************************************\
+  Name:			SMBus_handler.h
+  Author:   	Bryerton Shaw 	
+  Created: 		March 11, 2008
+  Description:	SMBus protocol using interrupts, created for FEB64 board
+
+  $Id$
+\**********************************************************************************/
+
 #ifndef  _SMBUS_HANDLER_H
 #define  _SMBUS_HANDLER_H
 
-#define WRITE_CYCLE 0
-#define READ_CYCLE  1
-#define ADD_READ    2
-#define ADD_WRITE   3
-#define CMD_CYCLE   4
+// SMBus Defines
+#define SMB_FREQUENCY	1000000	// in Hz, allowed range is 10kHz to 100kHz
 
-#define SMBUS_SI_WAITTIME 100 //in ms
+#define SMB_WRITE	0x00
+#define SMB_READ	0x01
 
+#define SMB_MAX_BUFF_SIZE	0x08
+
+#define SMB_MTSTA		0xE0
+#define SMB_MTDB		0xC0	
+#define SMB_MRDB		0x80
+ 
+// SMBus Global Variables
+unsigned char *pSMB_DATA_IN;
+unsigned char *pSMB_DATA_OUT;
+unsigned char SMB_DATA_LEN;
+unsigned char SMB_TARGET;
+
+bit SMB_BUSY;
+bit SMB_RW;
+bit SMB_SENDWORDADDR;
+bit SMB_RANDOMREAD;
+bit SMB_ACKPOLL;
+
+// Alias SDA and SCL to defined SMBus ports (see mscbemb.h)
+sbit SDA = MSCB_I2C_SDA;
+sbit SCL = MSCB_I2C_SCL;
+
+/***
+ * Function:	SMBus_Clear
+ * Purpose:		Attempt to clear SDA if a slave is holding SDA high due to 
+ *					improper SMBus reset or error.
+ *
+ * Notes:		Call on startup (before SMBus_Init)
+ */
+void SMBus_Clear(void);
+
+/***
+ * Function:	SMBus_Init
+ * Purpose:		Initialize SMBus Protocol
+ *
+ * Notes:		Configures Timer3, assumes Timer1 is set properly
+ */
 void SMBus_Init(void);
-bit SMBus_Start(void);
-bit SMBus_WriteByte(unsigned char Byte, unsigned char RW_flag);
-unsigned char SMBus_ReadByte(void);
-void SMBus_Stop(void);
-bit SMBus_SerialInterruptCheck(void);
 
-#endif
 
+
+#endif // _SMBUS_HANDLER_H
