05993d9919b885be0976540456e3a5a8325efa46
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-03-25 18:46:51 +0000

    Bahman Code has been cleaned and tested several times
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@137 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Devices/AT25160A.c
+++ Devices/AT25160A.c
@@ -16,17 +16,9 @@
 #include "../Protocols/SPI_handler.h"
 
 signed char ExtEEPROM_Init (unsigned char npages, unsigned char page_size)
-{
-//	unsigned char i;
-
-//	for (i=0;(i<npages)&&((page_size*i)<WP_START_ADDR);i++)
-//	{
-//		page_address[i]=page_size*i;
-//	}
-			 
+{	 
 	// Unselecting the device 
 	RAM_CSn = 1;
-	delay_us(10);
 
 	//Initializing the SPI
 	SPI_Init();
@@ -34,253 +26,164 @@ signed char ExtEEPROM_Init (unsigned char npages, unsigned char page_size)
 	if((page_size*npages)>WP_START_ADDR)
 		return ERROR;
 
-	return 0;
-}
-
-
-signed char ExtEEPROM_Write(unsigned int WritePage, void *source, unsigned char page_size)
-{
-		unsigned char i,j,WriteStatus;
-		unsigned char *src, counter=0;	
-
-		RAM_WPn = 0;
-		src = source;
-		WriteStatus=ExtEEPROM_Enable(WREN);
-		if (WriteStatus!=(WEN|PROTECTION))
-			return 5;
-		
-		for (i = 0; i <=(page_size /MAX_BYTE); i++) 
-		{  
-			
-		  	
-			RAM_CSn = 0;
-			SPI_WriteByte(WRITE); 					//Sending the Write Data to Memory Array command
-			WritePage +=(i* MAX_BYTE);					//Updating the address of mempry 
-			SPI_WriteUInt(WritePage);	 					//Sending the address of location that we want to do the write
-			
-			if (i == page_size /MAX_BYTE)
-			{	
-				counter = (page_size % MAX_BYTE);
-         }
-			else
-			{
-				counter = MAX_BYTE;
-			}
-			
-			for (j=0; j<counter; j++)
-			{	
-				SPI_WriteByte(*src);
-				src++;								
-			}
-
-			EEP_SCK = 0;								// Programming will start after the RAM_CSn is brought high. 	
-			RAM_CSn = 1;								// The low-to-high transition of the RAM_CSn must occur during 
-			delay_us(1);								// the SCK low-time immediately after clocking in the 
-															// D0 (LSB) data bit.
-
-			// Enabling the write operation, the device returns to write disable 
-			// state at the completion of a write ccycle (writing maximum 32 bytes)
-			RAM_CSn = 0;
-			WriteStatus=ExtEEPROM_Enable(WREN);
-			if (WriteStatus!=WEN)
-				return 6;
-		}
-	return 0;
-}
-
-signed char ExtEEPROM_Read (unsigned int ReadPage, void *destination, unsigned char page_size)
-{
-		
-	unsigned char ReadStatus,i=0;
-	unsigned char *des;
-			
-	des = destination;
+	ExtEEPROM_Wait();
 	
+	return 0;
+}
+
+signed char ExtEEPROM_Read (unsigned int ReadPage, unsigned char *destination, unsigned char page_size)
+{
+		
+	unsigned char i=0;
+			
 	//Making sure that Write Protection pin is high
-//	RAM_WPn = 1;	
-	ReadStatus=ExtEEPROM_Enable(WREN);
-	
-	if (ReadStatus != (WEN|PROTECTION))
-		return 4;
-
+	RAM_WPn = 1;	
+	ExtEEPROM_WriteEnable();
 
 	RAM_CSn = 0;
-	SPI_WriteByte(READ); 					//Sending the Write Data to Memory Array command
-	ReadPage +=(i* MAX_BYTE);					//Updating the address of mempry 
-	delay_us(10);
-	SPI_WriteUInt(ReadPage);	 					//Sending the address of location that we want to do the write
-	delay_us(1);
+	SPI_WriteByte(AT2516_READ); 					//Sending the Write Data to Memory Array command
+	SPI_WriteUInt(ReadPage);	 			//Sending the address of location that we want to do the write
 	
 	for (i;i<page_size;i++)
 	{
-		*des=SPI_ReadByteFalling();
-		des++;
-		delay_us(10);
+		*destination=SPI_ReadByteRising();
+		destination++;
+		
    }
+	delay_us(25);
 	RAM_CSn = 1;	
-	return 0;
-}
-
-signed char ExtEEPROM_Clear(unsigned int page, unsigned char page_size)
-{
-	
-		unsigned char i,j;
-		unsigned char counter=0;	
-
-		//Writing the protection section
-		if ( page+page_size > WP_START_ADDR )  
-		{
-				return ERROR;
-		}
-		
-		for (i = 0; i <=(page_size /MAX_BYTE); i++) 
-		{  
-			
-			// Enabling the write operation, the device returns to write disable state at the 
-			// completion of a write ccycle (writing maximum 32 bytes)
-			RAM_CSn = 0;
-			delay_us(1);
-			SPI_WriteByte(WREN);  					//Sending the Write Enable Latch command
-			RAM_CSn = 1;	  
-		  	delay_us(1);     
-
-			RAM_CSn = 0;
-			SPI_WriteByte(WRITE); 					//Sending the Write Data to Memory Array command
-			page +=(i* MAX_BYTE);					//Updating the address of mempry 
-			SPI_WriteUInt(page);	 					//Sending the address of location that we want to do the write
-			
-			if (i == page_size /MAX_BYTE)
-			{	
-				counter = (page_size % MAX_BYTE);
-         }
-			else
-			{
-				counter = MAX_BYTE;
-			}
-			
-			for (j=0; j<counter; j++)
-			{	
-				SPI_WriteByte(0);								
-			}
-
-			EEP_SCK = 0;								// Programming will start after the RAM_CSn is brought high. 	
-			RAM_CSn = 1;								// The low-to-high transition of the RAM_CSn must occur during 
-															// the SCK low-time immediately after clocking in the 
-															// D0 (LSB) data bit.
-		}
-
-return 0;
-}
-
-
-signed char ExtEEPROM_WriteProtect (void *source,unsigned char page_size)
-{
-	
-		unsigned char i,j,PWriteStatus;
-		unsigned int page;
-		unsigned char *src, counter=0;	
-
-		page = WP_START_ADDR;
-		src = source;
-		
-		//Making sure that Write Protection pin is high
-		RAM_WPn = 1;
-		RAM_CSn = 0;
-		PWriteStatus=ExtEEPROM_Enable(WREN);
-		RAM_CSn = 1;
-
-		//Writing to the Status register
-		RAM_CSn = 0;
-		SPI_WriteByte(WRSR);
-		SPI_WriteByte(WEN);
-		RAM_CSn = 1;
-		delay_us(1);
-		
-		RAM_CSn = 0;
-		PWriteStatus=ExtEEPROM_Enable(WREN);
-	//BS	if (PWriteStatus!=WEN)
-	//BS		return 1;
-		
-		for (i = 0; i <=(page_size /MAX_BYTE); i++) 
-		{  
-			
-		  	
-			RAM_CSn = 0;
-			SPI_WriteByte(WRITE); 					//Sending the Write Data to Memory Array command
-			page +=(i* MAX_BYTE);					//Updating the address of mempry 
-			SPI_WriteUInt(page);	 					//Sending the address of location that we want to do the write
-			
-			if (i == page_size /MAX_BYTE)
-			{	
-				counter = (page_size % MAX_BYTE);
-         }
-			else
-			{
-				counter = MAX_BYTE;
-			}
-			
-			for (j=0; j<counter; j++)
-			{	
-				SPI_WriteByte(*src);
-				src++;								
-			}
-
-			EEP_SCK = 0;								// Programming will start after the RAM_CSn is brought high. 	
-			RAM_CSn = 1;								// The low-to-high transition of the RAM_CSn must occur during 
-			delay_us(1);								// the SCK low-time immediately after clocking in the 
-															// D0 (LSB) data bit.
-
-			// Enabling the write operation, the device returns to write disable 
-			// state at the completion of a write ccycle (writing maximum 32 bytes)
-			RAM_CSn = 0;
-			PWriteStatus=ExtEEPROM_Enable(WREN);
-			delay_us(10);
-			RAM_CSn = 1;
-
-//BS			if (PWriteStatus!=WEN)
-//BS				return 2;
-		}
-
-	
-	/*Defining the protection region here */
-	
-	//Writing to the Status register
-	RAM_CSn = 0;
-	SPI_WriteByte(WRSR);
-	SPI_WriteByte(PROTECTION);
-	RAM_CSn = 1;
-	delay_us(10);
-	
-	RAM_CSn = 0;
-	PWriteStatus=ExtEEPROM_Enable(RDSR);
-//BS	if (PWriteStatus!=PROTECTION)
-//BS		return 3;
 	RAM_WPn = 0;
-	
-	RAM_CSn = 1;
 	return 0;
 }
 
-unsigned char ExtEEPROM_Enable(unsigned char flag)
-{
-	unsigned char status;		
+signed char ExtEEPROM_WriteProtect(unsigned char *source, unsigned char page_size)
+{	
+	unsigned char i,j;
+	unsigned int eeprom_addr, blockSize;
+	unsigned char counter = AT2516_MAX_BYTE;	
 	
-	if (flag==WREN)
-	{	
+	//Making sure that Write Protection pin is high
+	RAM_WPn = 1;
+
+	ExtEEPROM_WriteStatusReg(0x0);
+
+	blockSize = page_size / AT2516_MAX_BYTE;
+
+	for (i = 0; i <= blockSize; i++) {  
+		eeprom_addr = WP_START_ADDR + (i * AT2516_MAX_BYTE);					//Updating the address of mempry 
+		if (i == blockSize) {	
+			counter = (page_size % AT2516_MAX_BYTE);
+      }
+
+		ExtEEPROM_WriteEnable();
+		
+	
+		RAM_CSn = 0;
+	
+		SPI_WriteByte(AT2516_WRITE); 					//Sending the Write Data to Memory Array command
+		SPI_WriteUInt(eeprom_addr);	 					//Sending the address of location that we want to do the write
+						
+		for (j=0; j<counter; j++) {	
+			SPI_WriteByte(*source);
+			source++;								
+		}
+
+		delay_us(25);
+		RAM_CSn = 1;								// The low-to-high transition of the RAM_CSn must occur during 
+		delay_us(25);
+		ExtEEPROM_Wait();
+	}
+
+	ExtEEPROM_WriteStatusReg(AT2516_PROTECTION);
+	RAM_WPn = 0;	
+	return 0;
+}
+
+
+signed char ExtEEPROM_Write_Clear(unsigned int write_addr, unsigned char *source, 
+unsigned char page_size, unsigned char clear)
+{
+	unsigned char i,j;
+	unsigned int blockSize;
+	unsigned char counter = AT2516_MAX_BYTE;	
+	
+	//Making sure that Write Protection pin is high
+	RAM_WPn = 1;
+	blockSize = page_size / AT2516_MAX_BYTE;
+
+	for (i = 0; i <= blockSize; i++) {  
+		write_addr = write_addr + (i * AT2516_MAX_BYTE);					//Updating the address of mempry 
+		if (i == blockSize) {	
+			counter = (page_size % AT2516_MAX_BYTE);
+      }
+
+		ExtEEPROM_WriteEnable();
+		
+	
+		RAM_CSn = 0;
+	
+		SPI_WriteByte(AT2516_WRITE); 					//Sending the Write Data to Memory Array command
+		SPI_WriteUInt(write_addr);	 					//Sending the address of location that we want to do the write
+						
+		for (j=0; j<counter; j++) {	
+			if (!clear){
+				SPI_WriteByte(*source);
+				source++;
+			}
+			else
+			   SPI_WriteByte(0x00);										
+		}
+
+		delay_us(25);
+		RAM_CSn = 1;								// The low-to-high transition of the RAM_CSn must occur during 
+		delay_us(25);
+		ExtEEPROM_Wait();
+	}
+
+	RAM_WPn = 0;	
+	return 0;
+}
+
+void ExtEEPROM_WriteEnable(void)
+{				
 		//Enabling the write operation
 		RAM_CSn = 0;
 		delay_us(10);
-		SPI_WriteByte(WREN);  					
-		delay_us(10);
+		SPI_WriteByte(AT2516_WREN);  					
+		delay_us(25);
 		RAM_CSn = 1;
-		delay_us(10);		
-	}
-		
-	//Checking the status register
+		delay_us(25);
+		ExtEEPROM_Wait();
+}
+
+void ExtEEPROM_WriteStatusReg(unsigned char status) {
+	ExtEEPROM_WriteEnable();
 	RAM_CSn = 0;
-	SPI_WriteByte(RDSR);
-	status=SPI_ReadByteFalling();
+	SPI_WriteByte(AT2516_WRSR);
+	SPI_WriteByte(status);
+	delay_us(25);
 	RAM_CSn = 1;
-	delay_us(1);		
+	delay_us(25);
+	ExtEEPROM_Wait();
+}
+
+unsigned char ExtEEPROM_Status(void)
+{	
+	unsigned char status;
+
+	RAM_CSn = 0;
+	SPI_WriteByte(AT2516_RDSR);
+	status=SPI_ReadByteRising();
+	delay_us(25);
+	RAM_CSn = 1;
+	delay_us(10);		
+
 	return status;
+}
+
+void ExtEEPROM_Wait(void) {
+	unsigned char status;
+	do {			
+		status = ExtEEPROM_Status();
+	} while((status & 0x71) != 0);
 }
