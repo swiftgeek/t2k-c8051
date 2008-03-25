5a3e90a078e70b8fe116a47aec54bca2aa4da30d
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-03-25 18:47:38 +0000

    Bahman Code has been cleaned and tested several times
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@138 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Devices/AT25160A.h
+++ Devices/AT25160A.h
@@ -27,36 +27,38 @@ sbit RAM_HLDn = RAM_HOLD_DOWN;		 // The Hold Down Signal to puase the serial com
 sbit RAM_WPn  = RAM_WRITE_PROTECT;   // The Write Protection signal to enable/disable write to 
 												 // the status register
 
+// The Instruction Set for the AT25160A Device 
+#define AT2516_WREN  0x06  						//Set Write Enable Latch
+#define AT2516_WRDI  0x04  						//Reset Write Enable Latch
+#define AT2516_RDSR  0x05  						//Read Status Register
+#define AT2516_WRSR  0x01  						//Write Status Register
+#define AT2516_READ  0x03  						//Read Data from Memory Array
+#define AT2516_WRITE 0x02  						//Write Data to Memory Array
+
 #define WP_START_ADDR	0x0600
 #define WP_FINAL_ADDR	0x07FF
 
-// The Instruction Set for the AT25160A Device 
-#define WREN  0x06  						//Set Write Enable Latch
-#define WRDI  0x04  						//Reset Write Enable Latch
-#define RDSR  0x05  						//Read Status Register
-#define WRSR  0x01  						//Write Status Register
-#define READ  0x03  						//Read Data from Memory Array
-#define WRITE 0x02  						//Write Data to Memory Array
-
-
-#define MAX_BYTE 32
-#define PROTECTION 0x84					//Setting the bits of Status register: 
+#define AT2516_MAX_BYTE 32
+#define AT2516_PROTECTION 0x84					//Setting the bits of Status register: 
 												//Setting the BP0, WEN and WPEN to 1 and BP1 to 0.
 
 //unsigned int page_address[10] =0;
 
 // Defing the Status register's bits
-#define WEN   0x02
-#define WPEN  0x80
+#define AT2516_WEN   0x02
+#define AT2516_WPEN  0x80
 
 #define ERROR -1
 
 signed char ExtEEPROM_Init (unsigned char npages, unsigned char page_size);
-signed char ExtEEPROM_Read (unsigned int ReadPage, void *destination, unsigned char page_size);
-signed char ExtEEPROM_Write(unsigned int WritePage, void *source, unsigned char page_size);
-signed char ExtEEPROM_Clear(unsigned int ClearPage, unsigned char page_size);
-signed char ExtEEPROM_WriteProtect (void *source,unsigned char page_size);
-unsigned char ExtEEPROM_Enable(unsigned char flag);
-
+signed char ExtEEPROM_Read (unsigned int ReadPage, unsigned char *destination, 
+unsigned char page_size);
+signed char ExtEEPROM_Write_Clear(unsigned int WritePage, unsigned char *source, 
+unsigned char page_size, unsigned char clear);
+signed char ExtEEPROM_WriteProtect (unsigned char *source,unsigned char page_size);
+void ExtEEPROM_WriteEnable(void);
+void ExtEEPROM_WriteStatusReg(unsigned char status);
+unsigned char ExtEEPROM_Status(void);
+void ExtEEPROM_Wait(void);
 
 #endif
