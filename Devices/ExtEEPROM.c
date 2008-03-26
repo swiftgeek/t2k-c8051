e0ad57696d34e6bd4f2bbb93729b9634c5ed76d5
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-03-26 22:27:54 +0000

    Bahman Sotoodian The External EEPROM without the write_protect call
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@140 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

---
+++ Devices/ExtEEPROM.c
@@ -0,0 +1,220 @@
+/********************************************************************\
+
+  Name:         ExtEEPROM.c
+  Created by:   Bahman Sotoodian  								Feb/28/2007
+
+
+  Contents:     EEPROM (T25160AN-10SU-2.7) user interface
+
+  $Id:$
+
+\********************************************************************/
+//  need to have FEB64 defined
+
+#ifdef  _ExtEEPROM_
+
+#ifndef _SPI_PROTOCOL_
+#define _SPI_PROTOCOL_
+#endif
+
+//-----------------------------------------------------------------
+// Include files
+//-----------------------------------------------------------------
+#include "../mscbemb.h"
+#include "ExtEEPROM.h"
+#include "../Protocols/SPI_handler.h"
+
+//
+//-----------------------------------------------------------------
+/**
+Initializing the SPI communication protocol
+*/
+void ExtEEPROM_Init (void){	 
+	// Unselecting the device 
+	RAM_CSn = 1;
+
+	//Initializing the SPI
+	SPI_Init();
+}
+
+//
+//-----------------------------------------------------------------
+/**
+Reads data from ext_eeprom.
+
+@param ReadPage	   address of memory which originator wasnts to read from
+@param *destination  address of memory which the read data would be stored
+@param page_size		number of bytes of data that the originator wants to read from memory
+*/
+unsigned char ExtEEPROM_Read (unsigned int ReadPage, unsigned char *destination, 
+unsigned char page_size) {
+		
+	unsigned char i=0;
+			
+	//Making sure that Write Protection pin is high to start doing W/R operation
+	RAM_WPn = 1;	
+	ExtEEPROM_WriteEnable();
+
+	RAM_CSn = 0;
+	SPI_WriteByte(EEP_READcmd); 				//Sending the raed opcode 
+	SPI_WriteUInt(ReadPage);	 			//Sending the memory address 
+	
+	for (i;i<page_size;i++)	{
+		*destination=SPI_ReadByteRising();
+		destination++;
+   }
+
+	delay_us(EEP_delay);
+	RAM_CSn = 1;	
+	RAM_WPn = 0;
+	return EEP_SUCCESS;
+}
+
+//
+//-----------------------------------------------------------------
+/**
+Writes data to the ext_eeprom.
+@attention 							 The user should be able to set a bit in control register to request a write or clear
+						  			    operation to the ext_eeprom. The value of that bit should be reflected on the "clear" argument.
+@param write_addr					 The address of ext_eeprom's memory where the write/clear operation would be perfomred.
+@param *source                 The stored data which should be written in ext_eeprom
+@param page_size					 Number of bytes of data which will be written into ext_eeprom
+@param WC_flag						 Write or Clear flag
+@param *flag						 In case of EEP_BUSY return, the value of parameter 
+										 "i" would be stored in *flag for next function call
+@return EEP_PROTECTED		    Indicating that the memory is write protected and can not be modified
+@return EEP_BUSY					 Indiacting that the device is busy performing the internal write cycle
+@return EEP_SUCCESS  			 				  
+*/
+unsigned char ExtEEPROM_Write_Clear(unsigned int write_addr, unsigned char *source, 
+unsigned char page_size, unsigned char WC_flag, unsigned char *flag)
+{
+	unsigned char i,j;
+	unsigned int Nblock;
+	unsigned char counter = EEP_MAX_BYTE;		
+	
+	//Checking if we are trying to write in the write protected block
+	if (write_addr+page_size >= WP_START_ADDR)
+		return EEP_PROTECTED;
+
+	//Making sure that Write Protection pin is high
+	RAM_WPn = 1;
+	//Nblock would determine number of 32 bytes blocks
+	Nblock = page_size / EEP_MAX_BYTE;
+
+	for (i = *flag; i <= Nblock; i++) {  
+		//	To check whether eeprom is ready (not busy) or not
+		if(ExtEEPROM_Wait()==EEP_BUSY){
+			//Storing the value of counter("i")
+			*flag = i;
+			return EEP_BUSY;
+		}
+		//Updating the address of memory 				
+		write_addr = write_addr + (i * EEP_MAX_BYTE);					 
+		//Checking for the last cycle of write operation		
+		if (i == Nblock) {	
+			//Determing number of bytes left for the last cycle of write operation 
+			counter = (page_size % EEP_MAX_BYTE);
+      }
+		//Sending the Write Enable instruction 
+		ExtEEPROM_WriteEnable();					//The device will be automatically
+															//returned to the write disable state 
+															//at the completion of write cycle
+		
+	
+		RAM_CSn = 0;
+	
+		SPI_WriteByte(EEP_WRITEcmd); 					//Sending the WRITE opcode 
+		SPI_WriteUInt(write_addr);	 				//Sending the memory address which 
+															//data is going to be stored in
+						
+		for (j=0; j<counter; j++) {	
+			if (WC_flag == WRITE_EEPROM){
+				SPI_WriteByte(*source);
+				source++;
+			}
+			else
+			   SPI_WriteByte(CLEAR_EEPROM);		  //Clearing the memory									
+		}
+
+		delay_us(EEP_delay);							
+		RAM_CSn = 1;								 //Programming will start after the chip select 
+														 // is brought high	 
+		delay_us(EEP_delay);		
+	}
+
+	RAM_WPn = 0;	
+	return EEP_SUCCESS;
+}
+//
+//-----------------------------------------------------------------
+/**
+Sending the write enable instruction 
+*/
+void ExtEEPROM_WriteEnable(void)
+{				
+		//Enabling the write operation
+		RAM_CSn = 0;
+		delay_us(EEP_delay);
+		SPI_WriteByte(EEP_WRENcmd);  					
+		delay_us(EEP_delay);
+		RAM_CSn = 1;
+		delay_us(EEP_delay);
+		ExtEEPROM_Wait();
+}
+
+//
+//-----------------------------------------------------------------
+/**
+Writing to the status register
+@param status	Stuats would determine the value of WPEN,BP0,BP1 
+	            bits of status register
+*/
+void ExtEEPROM_WriteStatusReg(unsigned char status) {
+	ExtEEPROM_WriteEnable();
+	RAM_CSn = 0;
+	SPI_WriteByte(EEP_WRSRcmd);
+	SPI_WriteByte(status);
+	delay_us(EEP_delay);
+	RAM_CSn = 1;
+	delay_us(EEP_delay);
+	ExtEEPROM_Wait();
+}
+
+//
+//-----------------------------------------------------------------
+/**
+Reading the status register
+@attention	This function is not currently called, but it can be used to check the 
+			   state of statue register bits
+@return status	The value of status register
+*/
+unsigned char ExtEEPROM_Status(void)
+{	
+	unsigned char status;
+
+	RAM_CSn = 0;
+	SPI_WriteByte(EEP_RDSRcmd);
+	status=SPI_ReadByteRising();
+	delay_us(EEP_delay);
+	RAM_CSn = 1;
+	delay_us(EEP_delay);		
+
+	return status;
+}
+
+//
+//-----------------------------------------------------------------
+/**
+Checking to see whether the device is ready or not. 
+@return EEP_BUSY 	Indicating the device is busy performing internal write cycle
+@return EEP_READY  Indicating the device is ready for the next operation
+*/
+unsigned char ExtEEPROM_Wait(void) {
+	unsigned char status;		
+	status = ExtEEPROM_Status();
+	if((status & W_INTERNAL) != 0)
+		return EEP_BUSY;
+return EEP_READY;
+}
+#endif
---
+++ Devices/ExtEEPROM.h
@@ -0,0 +1,73 @@
+/*****************************************************************************
+/*
+  Name:         ExtEEPROM.h
+  Created by:   Bahman Sotoodian  								Feb/28/2008
+
+
+  Contents:     EEPROM user interface
+
+  
+\****************************************************************************/
+
+#ifndef  _ExtEEPROM_H_
+#define  _ExtEEPROM_H_
+
+//ExtEEPROM port assignment
+#define RAM_CHIP_SELECT    P3 ^ 7
+#define RAM_HOLD_DOWN      P2 ^ 2
+#define RAM_WRITE_PROTECT  P2 ^ 0
+
+sbit RAM_CSn  = RAM_CHIP_SELECT;     // The Chip Select Signal would select/unselect the chip
+sbit RAM_HLDn = RAM_HOLD_DOWN;		 // The Hold Down Signal would puase the serial communication			
+sbit RAM_WPn  = RAM_WRITE_PROTECT;   // The Write Protection signal would enable/disable write to 
+												 // the status register
+
+#define PAGE_SIZE				0xC8		 //Would define the length of our pages
+#define NUMEBER_PAGES		0x06		 //Would define number of pages  	
+
+//The EEP_CLEAR and EEP_WRITE would be passed to the ExtEEPROM_Write_Clear() function
+//to determine whether to perform the "clear" operation or the "write" one.
+#define CLEAR_EEPROM				0x00		 
+#define WRITE_EEPROM				0x01
+
+// The Instruction Set for the AT25160A Device 
+#define EEP_WRENcmd  0x06  			 //Set Write Enable Latch
+#define EEP_WRDIcmd  0x04  			 //Reset Write Enable Latch
+#define EEP_RDSRcmd  0x05  			 //Read Status Register
+#define EEP_WRSRcmd  0x01  			 //Write Status Register
+#define EEP_READcmd  0x03  			 //Read Data from Memory Array
+#define EEP_WRITEcmd 0x02  			 //Write Data to Memory Array
+
+//When the BP1 and BP0 has been set properly to 0 and 1 respectively 
+//(refere to AT25160AN manual)
+#define WP_START_ADDR	0x0600		//The first address of write protected block 
+#define WP_FINAL_ADDR	0x07FF		//The last address of write protected block
+
+
+#define EEP_MAX_BYTE 32				//Maximum number of bytes that can be written to memory 
+											//during each write cycle.
+#define EEP_PROTECTION 0x84		//Setting the bits of status register: 
+											//Setting the BP0, WEN and WPEN to 1 and BP1 to 0 to write 
+											//protect the last quarter of memory
+
+#define EEP_ERROR 	 	 0				
+#define EEP_SUCCESS 	 	 1
+#define EEP_READY			 1						
+#define EEP_PROTECTED 	 2	
+#define EEP_BUSY		 	 3
+#define EEP_delay    	25
+#define W_INTERNAL    0x71		  //Checking bit 3-6 of status register to see
+										  //whether device is busy performing the internal write 
+										  //cycle or is ready	
+
+void ExtEEPROM_Init (void);
+unsigned char ExtEEPROM_Read (unsigned int ReadPage, unsigned char *destination, 
+unsigned char page_size);
+unsigned char ExtEEPROM_Write_Clear(unsigned int WritePage, unsigned char *source, 
+unsigned char page_size, unsigned char WC_flag, unsigned char *flag);
+void ExtEEPROM_WriteEnable(void);
+void ExtEEPROM_WriteStatusReg(unsigned char status);
+unsigned char ExtEEPROM_Status(void);
+unsigned char ExtEEPROM_Wait(void);
+
+#endif
