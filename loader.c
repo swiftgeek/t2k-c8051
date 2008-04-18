f78b142c3752a78d1f3a0589aaa47d60e3f8dccb
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-04-18 20:10:21 +0000

    The ext_eeprom changed to loader
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@171 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

---
+++ loader.c
@@ -0,0 +1,125 @@
+/********************************************************************\
+ Name:         loader.c
+ Created by:   Bahman Sotoodian                Mar/11/2008
+ 
+ 
+ Contents:     Application specific (user) part of
+ 					Midas Slow Control Bus protocol
+ 					for External EEPROM of FEB64 board
+
+ Memory usage:  
+ define(_SPI_PROTOCOL_) 
+ 
+ 
+ $Id:$
+ \********************************************************************/
+ 
+ #include <stdio.h>
+ #include <math.h>
+ #include "mscbemb.h"
+ #include "loader.h"
+ #include "Devices/AT25160A.h"
+ 
+ 
+ //
+ // Global declarations
+ //-----------------------------------------------------------------------------
+ char code  node_name[] = "loader";
+ 
+ //
+ // Declare globally the number of sub-addresses to framework
+ unsigned char idata _n_sub_addr = 1;
+
+
+ // User Data structure declaration
+ //-----------------------------------------------------------------------------
+ MSCB_INFO_VAR code vars[] = {
+   1, UNIT_BYTE,            0, 0,           0, "Control",    &user_data.control,      // 0
+   1, UNIT_BYTE,            0, 0,           0, "EEPage",     &user_data.eepage,       // 1
+	1, UNIT_BYTE,            0, 0,           0, "Status",     &user_data.status,       // 0   
+   0
+ };
+
+ MSCB_INFO_VAR *variables = vars;   // Structure mapping
+ // Get sysinfo if necessary
+ extern SYS_INFO sys_info;          // For address setting
+ 
+
+ 
+ 
+ /********************************************************************\
+ Application specific init and in/output routines
+ \********************************************************************/
+ 
+ /*---- User init function ------------------------------------------*/
+ void user_init(unsigned char init)
+ {
+ 	char xdata add;
+
+ 	if (init)
+	{
+	 	// Initialize control and status
+   	user_data.control = 0;
+		user_data.status = 0;
+	}
+ 
+ //
+ // Initial setting for communication and overall ports (if needed).
+ //-----------------------------------------------------------------------------
+   SFRPAGE  = CONFIG_PAGE;
+   // P0MDOUT contains Tx in push-pull
+   P0MDOUT |= 0x20;   // add RS485_ENABLE in push-pull
+
+ //
+ // SPI Setting
+ //-----------------------------------------------------------------------------
+   SFRPAGE  = CONFIG_PAGE;
+   P3MDOUT |= 0x80; // Setting the RAM_CSn to push pull
+   P2MDOUT |= 0x18; // Setting the SPI_MOSI and SPI_SCK to push pull
+	P2MDOUT &= 0xFE; // Setting the RAM_WPn to open drain
+	
+	if(ExtEEPROM_Init(NUMEBER_PAGES,PAGE_SIZE))
+	{
+	  user_data.status = (1<<7);
+	}
+
+ 	add = cur_sub_addr();
+ }
+
+/*---- User write function -----------------------------------------*/
+ #pragma NOAREGS
+
+void user_write(unsigned char index) reentrant
+{
+	if(index);
+}
+ 
+ /*---- User read function ------------------------------------------*/
+ unsigned char user_read(unsigned char index)
+ {
+   if (index);
+   return 0;
+ }
+ 
+ /*---- User function called via CMD_USER command -------------------*/
+ unsigned char user_func(unsigned char *data_in, unsigned char *data_out)
+ {
+   /* echo input data */
+   data_out[0] = data_in[0];
+   data_out[1] = data_in[1];
+   return 2;
+ }
+ 
+//-----------------------------------------------------------------------------
+void user_loop(void) 
+{
+		signed char check=0;
+
+		check=ExtEEPROM_WriteProtect ((unsigned char*)&eepage, PAGE_SIZE);
+		check=ExtEEPROM_Read  (WP_START_ADDR,(unsigned char*)&eepage2, PAGE_SIZE);
+		check=ExtEEPROM_Write_Clear (0x0000,(unsigned char*)&test1, 
+		PAGE_SIZE,WRITE);
+		check=ExtEEPROM_Read  (0x0000,(unsigned char*)&test2, PAGE_SIZE);
+									
+	yield();
+} 
---
+++ loader.h
@@ -0,0 +1,85 @@
+/********************************************************************\
+
+  Name:         loader.h
+  Created by:   Bahman Sotoodian				
+
+  Contents:     Application specific (user) part of
+                Midas Slow Control Bus protocol
+                for External EEPROM of FEB64 test board
+
+  $Id:$
+
+\********************************************************************/
+#ifndef  _LOADER_H_
+#define  _LOADER_H_
+
+
+
+#define PAGE_SIZE			0xC8
+#define NUMEBER_PAGES	0x06
+#define CLEAR				0x01
+#define WRITE				0x00
+
+//Defining the page mapping
+
+unsigned int xdata page_addr[]={0x600,0x0,0x100,0x200,0x300,0x400,0x500};
+
+/*---- Define variable parameters returned to CMD_GET_INFO command ----*/
+struct user_data_type {
+	unsigned char control;       
+	unsigned char eepage;
+	unsigned char status;	     
+};
+
+struct user_data_type xdata user_data;
+
+struct EEPAGE {
+float lVIlimit[8]; // vQ iQ +6Vd +6Va -6Va -6Ia +6Ia +6Id 
+float uVIlimit[8];   
+float luCTlimit;
+float uuCTlimit;
+float lSSTlimit;
+float uSSTlimit;
+float lVQlimit;
+float uVQlimit;
+float lIQlimit;
+float uIQlimit;
+float lVBiaslimit;
+float uVBiaslimit;
+float lIBiaslimit;
+float uIBiaslimit;
+};
+
+//   vQ   iQ  +6Vd  +6Va  -6Va  -6Ia  +6Ia  +6Id 
+// uC Temperature
+// SST Temperature
+//   vQ    iQ  +6Vd  +6Va -6Va  -6Ia   +6Ia  +6Id 
+struct EEPAGE xdata eepage = {
+     30.0, 0.0, 5.5, 5.5, -6.5, -20.0, 5.0,   5.0
+    ,73.0, 0.1, 6.5, 6.5, -5.5, -1.0, 100.0, 200.0
+	 ,23., 45.
+    ,20., 30.
+};
+
+struct EEPAGE xdata eepage2 ;
+
+struct TEST{
+unsigned char Var1;
+unsigned char Var2;
+};
+
+struct TEST xdata test1 = {
+0x33,0x22
+};
+
+struct TEST xdata test2;
+/********************************************************************\
+  Application specific init and inout/output routines
+\********************************************************************/
+void user_init(unsigned char init);
+void user_loop(void);
+void user_write(unsigned char index) reentrant;
+unsigned char user_read(unsigned char index);
+unsigned char user_func(unsigned char *data_in, unsigned char *data_out);
+
+#endif
