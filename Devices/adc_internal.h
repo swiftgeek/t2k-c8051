e6f045cfb5ebaa55742a57f7c2fb61a71b12cd03
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-04-18 16:47:41 +0000

    The readVoltage has been moved to feb64
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@169 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Devices/adc_internal.h
+++ Devices/adc_internal.h
@@ -13,14 +13,10 @@
 #ifndef  _INTERNAL_ADC_H
 #define  _INTERNAL_ADC_H
 
-// External Vref
-#define VREF       2.432f
-
 // Number of active internal ADCs
 #define INTERNAL_N_CHN 8   // Number of Internal ADCs
 #define TCHANNEL       8   // Temperature channel
 void adc_internal_init(void);
 unsigned int adc_read(unsigned char channel);
-float read_voltage(unsigned char channel,unsigned int *rvalue);
 
 #endif
