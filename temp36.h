906382bde69df68d25beef64f58e3f752418bdcc
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-05-14 20:56:06 +0000

    Added/modified EEPROM code
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@189 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- temp36.h
+++ temp36.h
@@ -6,25 +6,57 @@
   Contents:     Application specific (user) part of
                 Midas Slow Control Bus protocol
                 for temp 36 board
-
+   $Id$
 \********************************************************************/
 
-unsigned char ADT7486A_addrArray[] = {0x48, 0x4A};
-float xdata temperature1, temperature2;
+unsigned char ADT7486A_addrArray[] = {0x50,0x4F,0x4E,0x4D,0x4C,0x4B, 0x4A,0x49,0x48};
+unsigned int xdata PageAddr[]={0x0,0x100,0x200,0x300,0x400,0x500};
+float xdata temperature;
 struct
 {
-  	unsigned char error[5];
+  	unsigned long error;
+	unsigned long error2;
 	unsigned long SerialN;
-	unsigned char control;       
+	unsigned char control;     
+	unsigned char eepage;   
 	unsigned char status;	
   	float         Temp[36];    // ADT7486A external2 temperature [degree celsius]
 }xdata user_data;
 
-/********************************************************************\
-  Application specific init and inout/output routines
-\********************************************************************/
-void user_init(unsigned char init);
-void user_loop(void);
-void user_write(unsigned char index) reentrant;
-unsigned char user_read(unsigned char index);
-unsigned char user_func(unsigned char *data_in, unsigned char *data_out);
+#define SST_LINE1 1
+#define SST_LINE2 2
+
+//Index value for the user data
+#define IDXCTL 	2
+
+// CTL register
+unsigned char bdata rCTL;
+sbit CeeS     = rCTL ^ 0;
+sbit CeeR     = rCTL ^ 1;
+sbit CeeClr   = rCTL ^ 2;
+
+
+// CSR Register
+unsigned char bdata rCSR;
+sbit SeeS     = rCSR ^ 0;
+sbit SeeR     = rCSR ^ 1;
+
+
+// Global ON / OFF definition
+#define CLEAR  0
+#define SET		1
+#define DONE   1
+#define FAILED 0
+
+struct EEPAGE {
+	float exttemp1[18];
+	float exttemp2[18];
+};
+
+struct EEPAGE xdata eepage={
+	10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,
+	20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20
+};
+
+//BS	For testing the EXTEEPROM
+struct EEPAGE xdata eepage2;
