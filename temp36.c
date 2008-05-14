243fa179d22c742d51b0163ca388c74f074b46ba
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-05-14 20:55:48 +0000

    Added/modified EEPROM code
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@188 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- temp36.c
+++ temp36.c
@@ -13,16 +13,26 @@
  #include <math.h>
  #include "mscbemb.h"
  #include "temp36.h"
- #include "Devices/temp36_ADT7486A_tsensor.h"
 
+ #ifdef _ADT7486A_
+ #include "Devices/temp36_ADT7486A_tsensor.h"
+ #endif
+
+ #ifdef _ExtEEPROM_
+ #include "Devices/ExtEEPROM.h"
+ #endif
 
   // Declare globally the number of sub-addresses to framework
  unsigned char idata _n_sub_addr = 1;
-
+ unsigned char bdata bChange;
  // Global declarations
  //-----------------------------------------------------------------------------
  char code  node_name[] = "temp36";
  char idata svn_rev_code[] = "$Rev$";
+ // Local flag
+ sbit AT7486A_FLAG = bChange ^ 0;
+ sbit EEP_W_FLAG   = bChange ^ 1;
+ sbit EEP_R_FLAG   = bChange ^ 2;
 
  // Get sysinfo if necessary
  extern SYS_INFO sys_info;          // For address setting
@@ -31,31 +41,32 @@
  // User Data structure declaration
  //-----------------------------------------------------------------------------
  MSCB_INFO_VAR code vars[] = {
-   4, UNIT_BYTE,            0, 0,           0, "SerialN",    &user_data.SerialN,      // 0
-   4, UNIT_BYTE,            0, 0,           0, "TError",     &user_data.error,        // 1
-   4, UNIT_BYTE,            0, 0,           0, "TError2",    &user_data.error2,       // 1
-   1, UNIT_BYTE,            0, 0,           0, "Control",    &user_data.control,      // 2
-   1, UNIT_BYTE,            0, 0,           0, "Status",     &user_data.status,       // 3
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp0-0",      &user_data.Temp[0],      // 4
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp0-1",      &user_data.Temp[1],      // 5
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp1-0",      &user_data.Temp[2],      // 6
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp1-1",      &user_data.Temp[3],      // 7
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp2-0",      &user_data.Temp[4],      // 8
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp2-1",      &user_data.Temp[5],      // 9
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp3-0",      &user_data.Temp[6],      // 10
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp3-1",      &user_data.Temp[7],      // 11
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp4-0",      &user_data.Temp[8],      // 12
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp4-1",      &user_data.Temp[9],      // 13
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp5-0",      &user_data.Temp[10],     // 14
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp5-1",      &user_data.Temp[11],     // 15
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp6-0",      &user_data.Temp[12],     // 16
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp6-1",      &user_data.Temp[13],     // 17
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp7-0",      &user_data.Temp[14],     // 18
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp7-1",      &user_data.Temp[15],     // 19
-	4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp8-0",      &user_data.Temp[16],     // 20
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp8-1",      &user_data.Temp[17],     // 21
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp9-0",      &user_data.Temp[18],     // 22
-   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp9-1",      &user_data.Temp[19],     // 23
+   4, UNIT_BYTE,            0, 0,           0, "SerialN",       &user_data.SerialN,      // 0
+   1, UNIT_BYTE,            0, 0,           0, "Control",       &user_data.control,      // 2
+   1, UNIT_BYTE,            0, 0,           0, "Status",        &user_data.status,       // 3
+	1, UNIT_BYTE,            0, 0,           0, "EEPage",        &user_data.eepage,       // 3
+   4, UNIT_BYTE,            0, 0,           0, "TError",        &user_data.error,        // 1
+   4, UNIT_BYTE,            0, 0,           0, "TError2",       &user_data.error2,       // 1
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp0-0",       &user_data.Temp[0],      // 4
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp0-1",       &user_data.Temp[1],      // 5
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp1-0",       &user_data.Temp[2],      // 6
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp1-1",       &user_data.Temp[3],      // 7
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp2-0",       &user_data.Temp[4],      // 8
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp2-1",       &user_data.Temp[5],      // 9
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp3-0",       &user_data.Temp[6],      // 10
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp3-1",       &user_data.Temp[7],      // 11
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp4-0",       &user_data.Temp[8],      // 12
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp4-1",       &user_data.Temp[9],      // 13
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp5-0",       &user_data.Temp[10],     // 14
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp5-1",       &user_data.Temp[11],     // 15
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp6-0",       &user_data.Temp[12],     // 16
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp6-1",       &user_data.Temp[13],     // 17
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp7-0",       &user_data.Temp[14],     // 18
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp7-1",       &user_data.Temp[15],     // 19
+	4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp8-0",       &user_data.Temp[16],     // 20
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp8-1",       &user_data.Temp[17],     // 21
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp9-0",       &user_data.Temp[18],     // 22
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp9-1",       &user_data.Temp[19],     // 23
    4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp11-0",      &user_data.Temp[20],     // 24
    4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp11-1",      &user_data.Temp[21],     // 25
    4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "Temp12-0",      &user_data.Temp[22],     // 26
@@ -97,10 +108,10 @@ void user_init(unsigned char init)
                            (svn_rev_code[8]-'0')*10+
                            (svn_rev_code[9]-'0');
 
-
  // Initial setting for communication and overall ports (if needed).
  //-----------------------------------------------------------------------------
   //ADT7486A Init
+#ifdef _ADT7486A_
   SFRPAGE  = CONFIG_PAGE;
   P0MDOUT |= 0x18; // Setting the SST_DRV (SST) to push pull
   SFRPAGE  = CPT1_PAGE;
@@ -109,8 +120,7 @@ void user_init(unsigned char init)
   SFRPAGE  = CPT0_PAGE;
   CPT0CN  |= 0x80; // Enable the Comparator 0
   CPT0MD   = 0x03; //Comparator0 Mode Selection
-   //Use default, adequate TYP (CP1 Response Time, no edge triggered interrupt)
-	  
+   //Use default, adequate TYP (CP1 Response Time, no edge triggered interrupt)	  
   ADT7486A_Init(SST_LINE1); //Temperature measurements related initialization
   ADT7486A_Init(SST_LINE2); 
   //State initialization
@@ -120,12 +130,28 @@ void user_init(unsigned char init)
   for (i=0; i<36;i ++)
       user_data.Temp[i] = 0.0;
   ENABLE_INTERRUPTS
+#endif
+
+  //External EEPROM chip
+#ifdef _ExtEEPROM_
+  SFRPAGE  = CONFIG_PAGE;
+  P2MDOUT |= 0x31; // Setting the RAM_CSn. SPI_MOSI, SPI_SCK to push pull
+  P2MDOUT &= 0xFB; // Setting the RAM_WPn to open drain
+  ExtEEPROM_Init();
+#endif
+  rCTL=0;
+  rCSR=0;
 }
 
 /*---- User write function -----------------------------------------*/
 
-void user_write(unsigned char index) reentrant;
-
+void user_write(unsigned char index) reentrant
+{
+	rCSR=user_data.status;
+	if(index==IDXCTL)
+		rCTL=user_data.control;
+    
+}
 /*---- User read function ------------------------------------------*/
 
 unsigned char user_read(unsigned char index)
@@ -148,7 +174,11 @@ unsigned char user_func(unsigned char *data_in, unsigned char *data_out)
 /*---- User loop function ------------------------------------------*/
 void user_loop(void)
 {
-   unsigned char idata channel;
+   unsigned char idata channel, eeprom_wstatus, eeprom_rstatus;
+	unsigned int xdata eeptemp_addr;
+	unsigned char xdata *eeptemp_source, eep_request;
+	static unsigned char eeprom_flag=CLEAR; 
+#ifdef _ADT7486A_
 	//Read the first external temperature from each chip that is connected to SST_LINE1
    for (channel=0;channel < 9; channel++)
 	{
@@ -157,31 +187,31 @@ void user_loop(void)
          
          DISABLE_INTERRUPTS;
          user_data.Temp[channel*2] = temperature;
-         user_data.error   = user_data.error|(CLEAR<<channel);
+         user_data.error   |= (CLEAR<<channel);
          ENABLE_INTERRUPTS;
       }
       else
       {
          DISABLE_INTERRUPTS;
-         user_data.error   = user_data.error|(SET<<channel);;
+         user_data.error   |= (SET<<channel);;
          ENABLE_INTERRUPTS;
       }
    }
 	//Read the first external temperature from each chip that is connected to SST_LINE2
    for (channel=9;channel < 18; channel++)
 	{
-      if(!ADT7486A_Cmd(ADT7486A_addrArray[channel], GetExt1Temp, &temperature, SST_LINE2))
+      if(!ADT7486A_Cmd(ADT7486A_addrArray[channel-9], GetExt1Temp, &temperature, SST_LINE2))
       {
          
          DISABLE_INTERRUPTS;
          user_data.Temp[channel*2] = temperature;
-         user_data.error   = user_data.error|(CLEAR<<channel);
+         user_data.error   |= (CLEAR<<channel);
          ENABLE_INTERRUPTS;
       }
       else
       {
          DISABLE_INTERRUPTS;
-         user_data.error   = user_data.error|(SET<<channel);;
+         user_data.error   |= (SET<<channel);;
          ENABLE_INTERRUPTS;
       }
    }
@@ -192,35 +222,87 @@ void user_loop(void)
       {
          DISABLE_INTERRUPTS;
          user_data.Temp[(channel*2)+1] = temperature;
-         user_data.error2   = user_data.error2|(CLEAR<<channel);
+         user_data.error2   |= (CLEAR<<channel);
          ENABLE_INTERRUPTS;
       }
       else
       {
          DISABLE_INTERRUPTS;
-         user_data.error   = user_data.error2|(CLEAR<<channel);
+         user_data.error2   |= (SET<<channel);
          ENABLE_INTERRUPTS;
       }
    }
 	//Read the second external temperature from each chip that is connected to SST_LINE2
    for (channel=9;channel < 18; channel++)
 	{
-      if(!ADT7486A_Cmd(ADT7486A_addrArray[channel], GetExt2Temp, &temperature,SST_LINE2))
+      if(!ADT7486A_Cmd(ADT7486A_addrArray[channel-9], GetExt2Temp, &temperature,SST_LINE2))
       {
          DISABLE_INTERRUPTS;
          user_data.Temp[(channel*2)+1] = temperature;
-         user_data.error2   = user_data.error2|(CLEAR<<channel);
+         user_data.error2   |= (CLEAR<<channel);
          ENABLE_INTERRUPTS;
       }
       else
       {
          DISABLE_INTERRUPTS;
-         user_data.error   = user_data.error2|(CLEAR<<channel);
+         user_data.error2   |= (SET<<channel);
          ENABLE_INTERRUPTS;
       }
    }
+#endif
+#ifdef _ExtEEPROM_
+   if (CeeS) 
+	{
+       //Check if we are here for the first time
+      if (!eeprom_flag){
+         rCSR = user_data.status;
+         //Temporary store the first address of page
+         eeptemp_addr = PageAddr[(unsigned char)(user_data.eepage & 0x07)];
+         //Temporary store the first address of data which has to be written
+         eeptemp_source = (unsigned char*)&eepage;
+      }
+      //EPROM clear request
+      if (CeeClr)
+         eep_request = CLEAR_EEPROM;
+      else 
+         eep_request = WRITE_EEPROM;
+      
+      eeprom_wstatus = ExtEEPROM_Write_Clear (eeptemp_addr,&(eeptemp_source),
+      PAGE_SIZE,eep_request,&eeprom_flag);
 
+      if (eeprom_wstatus == DONE) {
+         SeeS = DONE;
+         eeprom_flag = CLEAR;
+         CeeS = CLEAR;
+         //Set the active page
+         user_data.eepage |= ((user_data.eepage & 0x07) <<5);
+      } 
+		else 
+         SeeS = FAILED;
+ 
+      // Publish Registers state
+      DISABLE_INTERRUPTS;
+      user_data.control = rCTL;
+      user_data.status  = rCSR;
+      ENABLE_INTERRUPTS;
+   }
+   if (CeeR) {
+      rCSR = user_data.status;
+      eeprom_rstatus = ExtEEPROM_Read  (PageAddr[(unsigned char)(user_data.eepage & 0x07)],
+      (unsigned char*)&eepage2, PAGE_SIZE);
+      if (eeprom_rstatus == DONE){
+         CeeR = CLEAR;
+         SeeR = DONE;
+      } 
+		else
+         SeeR = FAILED;
 
+      // Publish Registers state
+      DISABLE_INTERRUPTS;
+      user_data.control = rCTL;
+      user_data.status  = rCSR;
+      ENABLE_INTERRUPTS;
+   }
+#endif
  	led_blink(1, 1, 250);
-
 }
