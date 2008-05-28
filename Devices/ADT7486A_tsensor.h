3a21c4775458c08be399d319cb9c4cf7a26db618
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-05-28 23:37:41 +0000

    Modified ADT functions to support more than one SST line
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@204 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Devices/ADT7486A_tsensor.h
+++ Devices/ADT7486A_tsensor.h
@@ -1,15 +1,13 @@
 /**********************************************************************************\
-  Name:         ADT7486A_tsensor.h
+  Name:         temp36_ADT7486A_tsensor.h
   Created by:   Brian Lee						     May/11/2007
   Modified by:  Bahman Sotoodian					  March/03/2008
-
+  Modified by:  Noel Wu                        May/12/2008
 
   Contents:     Temperature sensor array (ADT7486A) handler for feb64 board
 
   $Id$
 \**********************************************************************************/
-
-#ifdef _ADT7486A_
 
 #ifndef  _ADT7486A_TSENSOR_H
 #define  _ADT7486A_TSENSOR_H
@@ -25,15 +23,15 @@
 // (3) readLength
 // (4) command
 // (5) data (if applicable, defined by user), 
-//                    cmd   wLen  rLen  MSB  LSB
-#define Ping          0x00, 0x00, 0x00, 0x00, 0x00
-#define GetIntTemp    0x00, 0x01, 0x02, 0x00, 0x00
-#define GetExt1Temp   0x01, 0x01, 0x02, 0x00, 0x00
-#define GetExt2Temp   0x02, 0x01, 0x02, 0x00, 0x00
-#define GetAllTemps   0x00, 0x01, 0x06
-#define SetExt1Offset 0xE0, 0x03, 0x00, 0x00, 0x00
-#define GetExt1Offset 0xE0, 0x01, 0x02, 0x00, 0x00
-#define SetExt2Offset 0xE1, 0x03, 0x00, 0x00, 0x00
+//                    cmd   wLen  rLen  MSB   LSB  
+#define Ping          0x00, 0x00, 0x00, 0x00, 0x00		
+#define GetIntTemp    0x00, 0x01, 0x02, 0x00, 0x00		
+#define GetExt1Temp   0x01, 0x01, 0x02, 0x00, 0x00  	
+#define GetExt2Temp   0x02, 0x01, 0x02, 0x00, 0x00		
+#define GetAllTemps   0x00, 0x01, 0x06						
+#define SetExt1Offset 0xE0, 0x03, 0x00
+#define GetExt1Offset 0xE0, 0x01, 0x02, 0x00, 0x00		
+#define SetExt2Offset 0xE1, 0x03, 0x00
 #define GetExt2Offset 0xE1, 0x01, 0x02, 0x00, 0x00
 #define ResetDevice   0xF6, 0x01, 0x00
 
@@ -65,15 +63,13 @@
 #define CONVERSION_FACTOR1 0.9294f 	// Should be 1.0 (-PAA- to be verified)
 #define CONVERSION_FACTOR2 0.8569f  // Only offset is required (in principle)
 
-void ADT7486A_Init(void);
+void ADT7486A_Init(int SST_LINE);
 signed char ADT7486A_Cmd(unsigned char addr, unsigned char command
                        , unsigned char writeLength, unsigned char readLength 
 					   , unsigned char datMSB, unsigned char datLSB
-					   , float *fdata);
+					   , int SST_LINE, float *fdata);
 signed char ADT7486A_Read(unsigned char writeFCS_Originator
                         , unsigned char cmdflag
-                        , float *temperature);
+                        , int SST_LINE, float *temperature);
 
 #endif // _ADT7486A_TSENSOR_H
-
-#endif // _ADT7486A_
