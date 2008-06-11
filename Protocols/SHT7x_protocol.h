ae68f76e392f975ab45f2bdef50a7c1281d1ef43
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-06-11 23:35:32 +0000

    Modified the code to support more than one humidity sensors
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@218 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- Protocols/SHT7x_protocol.h
+++ Protocols/SHT7x_protocol.h
@@ -10,9 +10,11 @@
 #ifndef _SHT7x_PROTOCOL_H
 #define _SHT7x_PROTOCOL_H
 
-sbit SHT7x_DATA = SHT_DATA;
-sbit SHT7x_SCK = SHT_SCK;
+sbit SHT7x_DATA1 = SHT_DATA1;
+sbit SHT7x_SCK1 = SHT_SCK1;
 
+sbit SHT7x_DATA2 = SHT_DATA2;
+sbit SHT7x_SCK2 = SHT_SCK2;
 //#define SHT7x_DELAY	1 //in us
 #define SHT7x_DELAY       5
 #define SHT7x_SHORT_DELAY 0.3
@@ -21,14 +23,14 @@ sbit SHT7x_SCK = SHT_SCK;
 #define ERROR 0
 #define DONE  1
 
-void SHT7x_Init(void); 
-void SHT7x_ClockOnce(void);
-void SHT7x_ConnectionReset(void);
-void SHT7x_TransStart(void);
+void SHT7x_Init(int humsen); 
+void SHT7x_ClockOnce(int humsen);
+void SHT7x_ConnectionReset(int humsen);
+void SHT7x_TransStart(int humsen);
 unsigned char SHT7x_FCS_Step(unsigned int msg, unsigned char fcs) reentrant; 
-unsigned char SHT7x_WriteByte(unsigned char dataToSend);
-unsigned char SHT7x_ReadByte(unsigned char read_flag);
+unsigned char SHT7x_WriteByte(unsigned char dataToSend, int humsen);
+unsigned char SHT7x_ReadByte(unsigned char read_flag, int humsen);
 //NW Reading from the Data line, read before the falling edge to 
 //compensate for the slow rise time of the weak pull up resistor
-void SHT7x_ClockOnce2(void);
+void SHT7x_ClockOnce2(int humsen);
 #endif
