a2793d6d37654f797ad10eb6bd929a0e8a8e654c
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-08-22 20:33:12 +0000

    made changes to the port pins of LPB
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@330 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- mscbemb.h
+++ mscbemb.h
@@ -604,7 +604,7 @@ sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 #include <c8051F120.h>
 #define CPU_C8051F120
 
-#define LED_0 P2 ^ 7 
+#define LED_0 P0 ^ 7 
 #if defined(LED_1) 
 #undef LED_1
 #endif
@@ -612,14 +612,14 @@ sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 #define LED_ON 0 //defines LED "ON" as forcing the specified pin to low
 
 //The pins that are used for Threshold voltages for comparator 0 and comparator 1
-#define MSCB_SST1 P1 ^ 3 //SST1 line  SST_IO (Write/Push-Pull)
+#define MSCB_SST1 P1 ^ 0 //SST1 line  SST_IO (Write/Push-Pull)
 #define SST_ClientResponse1 (char) ((CPT1CN & 0x40) >> 6) //Comparator1 overflow bit
 
 // SPI
-#define MSCB_SPI_SCK  P2 ^ 2
-#define MSCB_SPI_MOSI P2 ^ 3
-#define MSCB_SYNC     P2 ^ 1
-#define MSCB_SPI_MISO P2 ^ 0
+#define MSCB_SPI_SCK  P2 ^ 3
+#define MSCB_SPI_MOSI P2 ^ 4
+#define MSCB_SYNC     P1 ^ 1
+#define MSCB_SPI_MISO P2 ^ 1
 
 sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 /*--------------------------------*/
