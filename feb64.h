2852f10aaa162115d907a5086ccb11d08775ca76
fgddaq <fgddaq@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-08-28 23:12:41 +0000

    include calQpump in the structure
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@360 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- feb64.h
+++ feb64.h
@@ -97,12 +97,12 @@ unsigned int Offst;
 struct ADC2MSCB_TABLE xdata adc2mscb_table[16] = {
   {GAIN32, 1, I_A2MTABLE, 10000, 22}, {GAIN32, 3, I_A2MTABLE, 10000, 127}
 , {GAIN32, 5, I_A2MTABLE, 10000, 7176}, {GAIN32, 7, I_A2MTABLE, 10000, 1840}
-, {GAIN1 , 7, V_A2MTABLE,   101, 0}, {GAIN1 , 5, V_A2MTABLE, 101, 0}
-, {GAIN1 , 3, V_A2MTABLE,   101, 0}, {GAIN1 , 1, V_A2MTABLE, 101, 0}
+, {GAIN1 , 7, V_A2MTABLE,   101, 55}, {GAIN1 , 5, V_A2MTABLE, 101, 55}
+, {GAIN1 , 3, V_A2MTABLE,   101, 55}, {GAIN1 , 1, V_A2MTABLE, 101, 55}
 , {GAIN32, 0, I_A2MTABLE, 10000, 3178}, {GAIN32, 2, I_A2MTABLE, 10000, 1800}
 , {GAIN32, 4, I_A2MTABLE, 10000, 925}, {GAIN32, 6, I_A2MTABLE, 10000, 4895}
-, {GAIN1 , 6, V_A2MTABLE,   101, 0}, {GAIN1 , 4, V_A2MTABLE, 101, 0}
-, {GAIN1 , 2, V_A2MTABLE,   101, 0}, {GAIN1 , 0, V_A2MTABLE, 101, 0}
+, {GAIN1 , 6, V_A2MTABLE,   101, 55}, {GAIN1 , 4, V_A2MTABLE, 101, 55}
+, {GAIN1 , 2, V_A2MTABLE,   101, 55}, {GAIN1 , 0, V_A2MTABLE, 101, 55}
 };
 #define CONVER_FAC1 0
 #endif
@@ -166,7 +166,9 @@ unsigned int structsze;
 unsigned int rasum[8];
 unsigned int rqpump;
 unsigned int SWbias;
-unsigned char rbias [64];
+unsigned int calQpump; 
+unsigned char rbias [64];  
+// back door access starts @ 92 ->offset=23 (0x17)
 float lVIlimit[8]; // vQ iQ +6Vd +6Va -6Va -6Ia +6Ia +6Id 
 float uVIlimit[8];
 float luCTlimit, uuCTlimit;
@@ -177,22 +179,24 @@ float lVBiaslimit, uVBiaslimit;
 float lIBiaslimit, uIBiaslimit;
 float ext1offset[4];
 float ext2offset[4];
-float iBiasOffset[8]; 
+float iBiasOffset[8];
 };
 
 // Default structure 
 struct EEPAGE xdata eepage = {
-// 44 - Card Serial Number
+// 0x0 - Card Serial Number
    0x00000000
-// Structure size
+// - Structure size
    ,110
-// 45 - Asum[0..7] threshold
+// - Asum[0..7] threshold
 	 ,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff
-// 53 - Qpump DAC
+// - Qpump DAC
 	 ,0x0000
-// 54 - Qpump switch
+// - Qpump switch
 	 ,0x0000
-// 55 - DACs[0..63]
+// - Qpump DAC value for Calibration
+   , 800
+// - DACs[0..63]
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
@@ -201,27 +205,27 @@ struct EEPAGE xdata eepage = {
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
 	 ,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
-// 0 - LvQ, LiQ,  Lp6Vd, Lp6Va, Ln6Va , Lp6Ia, Lp6Ia , Lp6Id 
+// 0x17 - LvQ, LiQ,  Lp6Vd, Lp6Va, Ln6Va , Lp6Ia, Lp6Ia , Lp6Id 
    ,30.0, 0.0, 5.5, 5.5, -6.5, 0.0, 0.0, 0.0
-// 8 - HvQ, HiQ, Hp6Vd, Hp6Va, Hp6Va,  Hp6Ia, Hp6Ia,  Hp6Id 
+// 0x1F - HvQ, HiQ, Hp6Vd, Hp6Va, Hp6Va,  Hp6Ia, Hp6Ia,  Hp6Id 
    ,73.0, 0.1, 6.5, 6.5, -5.5, 0.2, 0.8, 0.2
-// 16 - LuC Temperature,  HuC Temperature
-	 ,23., 45.
-// 18 - LSST Temperature,  HSST Temperature
-   ,20., 30.
-// 20 - LVQ, HVQ (V)
-	 ,-1.0,1.0
-// 22 - LVQ, HVQ (V)
-	 ,-0.1,1.0
-// 24 - LVBias, HVBias (V)
-	 ,0.0,73.0
-// 26 - LIBias, LVBias (uA)
-	 ,0.0,10.0
-// 28 - SST channel 1 offset[0..3]
-	 ,0,0,0,0
-// 32 - SST channel 2 offset[0..3]
-	 ,0,0,0,0
-// 36 - Current Bias Offset [0..7]
+// 0x27 - LuC Temperature,  HuC Temperature
+	 ,18., 50.
+// 0x29 - LSST Temperature,  HSST Temperature
+   ,18. ,30.
+// 0x2B - LVQ, HVQ (V)
+	 ,-1.0 ,1.0
+// 0x2D - LVQ, HVQ (V)
+	 ,-0.1 ,1.0
+// 0x2F - LVBias, HVBias (V)
+	 ,0.0 ,73.0
+// 0x31 - LIBias, LVBias (uA)
+	 ,0.0 ,10.0
+// 0x33 - SST channel 1 offset[0..3]
+	 ,0,0 ,0,0
+// 0x35 - SST channel 2 offset[0..3]
+	 ,0,0 ,0,0
+// 0x37 - Current Bias Offset [0..7]
    , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 };
 
