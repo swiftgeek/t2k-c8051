de0dda7bb803a5f7d72a238d977a38d2f0838cf8
midas <midas@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2008-09-11 05:39:45 +0000

    Cosmetics move S/N to top change to 4 PAGEs
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@391 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- temp36.h
+++ temp36.h
@@ -1,7 +1,7 @@
 /********************************************************************\
 
   Name:         temp36.h
-  Created by:   Noel Wu				
+  Created by:   Noel Wu
 
   Contents:     Application specific (user) part of
                 Midas Slow Control Bus protocol
@@ -9,72 +9,78 @@
    $Id$
 \********************************************************************/
 
+#ifndef  _TEMP36_H_
+#define  _TEMP36_H_
+
 /*****USER DEFINED DATA*****/
 struct {
-	unsigned long SerialN;		//Serial Number
-	unsigned char control; 		//Writing/Reading the EEPROM
-	unsigned char status;  		//Displaying the status of the EEPROM command
-	unsigned char eepage;  		//EEPROM page number
-	unsigned char terror[5]; 	//Status for reading the temperature
-  	float         Temp[36];    //ADT7486A external temperature [degree celsius]
-   unsigned long eepValue;		//EEPROM Value to be stored/read
-	long 			  eeCtrSet;	   //Initiate changing the offset values
-   float			  SHTtemp1;
-   float  		  SHThumi1;
-   float			  SHTtemp2;
-   float  		  SHThumi2;
-   unsigned int  DAC[8];
-	unsigned int  navg;
-	float 		  AT[36];
-	float			  ref;
-} xdata user_data;			
+  unsigned long SerialN;    //Serial Number
+  unsigned char control;    //Writing/Reading the EEPROM
+  unsigned char status;     //Displaying the status of the EEPROM command
+  unsigned char eepage;     //EEPROM page number
+  unsigned char terror[5];  //Status for reading the temperature
+  float         Temp[36];   //ADT7486A external temperature [degree celsius]
+  unsigned long eepValue;   //EEPROM Value to be stored/read
+  long        eeCtrSet;     //Initiate changing the offset values
+  float        SHTtemp1;
+  float        SHThumi1;
+  float        SHTtemp2;
+  float        SHThumi2;
+  unsigned int  DAC[8];
+  unsigned int  navg;
+  float       AT[36];
+  float       ref;
+} xdata user_data;
 
 
 /*****TEMPERATURE VARIABLES*****/
 #define SST_LINE1 1
 #define SST_LINE2 2
 //Address for all the ADT7486A sensors on the SST_LINE1/SST_LINE2
-unsigned char xdata ADT7486A_addrArray[] = {0x48,0x49,0x4A,0x4B,0x4C,0x4D, 0x4E,0x4F,0x50};
+unsigned char xdata ADT7486A_addrArray[] = {0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50};
+
 //Every element in Terrorclear indicates a particular binary bit is 0
 unsigned char xdata Terrorclear[]={0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F};
+
 //Every element in Terrorclear indicates a particular binary bit is 1
 unsigned char xdata Terrorset[]={0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
 float xdata Taverage[36][30];
 #define TAVGMAX 30
 
 /*****EEPROM VARIABLES*****/
-//The EEPROM is separated into a total of 6 pages
-unsigned int xdata PageAddr[]={0x0,0x100,0x200,0x300,0x400,0x500};
+//The EEPROM is separated into a total of 4pages (0x600==WP)
+unsigned int xdata PageAddr[]={0x0, 0x200, 0x400, 0x600};
 //The structure of each EEPAGE
 struct EEPAGE {
-	int ext1offset[18];
-	int ext2offset[18];
-
-	int control;
-	unsigned long SerialN;
+  unsigned long SerialN;
+  int ext1offset[18];
+  int ext2offset[18];
+  int control;
 };
+
 //+/- 16 increments corresponds to a +/- 0.25 degrees offset in the ADT7486A chip
 //Initial values for eepage
 struct EEPAGE xdata eepage={
-	//Temperature sensor offsets corresponding to the external 1 channel
+  0,     // S/N
+  //Temperature sensor offsets corresponding to the external 1 channel
     //Temp02,04....36
-	32,48,32,32,16,16,48,16,0,16,0,0,16,32,16,32,-16,32 ,
-	//Temperature sensor offsets corresponding to the external 2 channel
-    //Temp01,03....35
-	48,32,48,64,64,32,48,64,0,16,16,32,48,48,32,32,16,32,
-
-	0,
-	1000
+  32, 48, 32, 32, 16, 16, 48, 16, 0, 16, 0, 0, 16, 32, 16, 32, -16, 32,
+  //Temperature sensor offsets corresponding to the external 2 channel
+  //Temp01,03....35
+  48, 32, 48, 64, 64, 32, 48, 64, 0, 16, 16, 32, 48, 48, 32, 32, 16, 32,
+  1000  // Control
 };
-//Keys for changing the EEPROM
-#define EEP_CTRL_KEY	    0x3C000000
-#define EEP_CTRL_READ    0x00110000  	
-#define EEP_CTRL_WRITE 	 0x00220000
-#define EEP_CTRL_INVAL_REQ -100	
-#define EEP_CTRL_INVAL_KEY -10	
-#define SERIALN_ADD  0x64A  
-#define TEMPOFF_LAST_INDX 36
 
+#define PAGE_SIZE  sizeof(eepage)  // Macro
+
+//Keys for changing the EEPROM
+#define EEP_CTRL_KEY        0x3C000000
+#define EEP_CTRL_READ       0x00110000
+#define EEP_CTRL_WRITE      0x00220000
+#define EEP_CTRL_INVAL_REQ -100
+#define EEP_CTRL_INVAL_KEY -10
+#define SERIALN_ADD         0x64A
+#define TEMPOFF_LAST_INDX   36
 
 /*****HUMIDITY SENSOR VARIABLES*****/
 #define humsen1 1
@@ -84,21 +90,18 @@ sbit SHT_VDD1=P1^1;
 sbit SHT_GND2=P1^7;
 sbit SHT_VDD2=P1^5;
 
-
 /*****DAC VARIABLES******/
-#define First_DACIndex    52
-#define Last_DACIndex	  59
+#define First_DACIndex   52
+#define Last_DACIndex    59
 
-	 
 /*****GENERAL*****/
-
 //Control Register
 unsigned char bdata rCTL;
 sbit CeeS     = rCTL ^ 0;
 sbit CeeR     = rCTL ^ 1;
 sbit CeeClr   = rCTL ^ 2;
 sbit CHum     = rCTL ^ 3;
-sbit Cref	  = rCTL ^ 4;	
+sbit Cref     = rCTL ^ 4;
 
 // Status Register
 unsigned char bdata rCSR;
@@ -108,17 +111,17 @@ sbit Sref     = rCSR ^ 4;
 
 
 //Index value for the user data
-#define IDXCTL          	1
-#define IDXEEP_CTL	     47
-#define IDXNAVG 		  	   4
-#define IDXREF 			  96
+#define IDXCTL         1
+#define IDXEEP_CTL    47
+#define IDXNAVG        4
+#define IDXREF        96
+
 // Global ON / OFF definition
 #define DONE   1
 #define FAILED 0
 #define SET    1
 #define CLEAR  0
-
-
+#endif
 
 
 
