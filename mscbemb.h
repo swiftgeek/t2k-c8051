137cb4ebad8050344f21101e088cba3ec8c3684c
midas <midas@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2009-01-13 00:46:53 +0000

    Add L_CMB adjust order of cards
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@543 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- mscbemb.h
+++ mscbemb.h
@@ -475,7 +475,7 @@ sbit P2_1 = SST1_REF; // the bit that controls and reads from P1 ^ 1
 
 sbit RS485_ENABLE = P0 ^ 3; //MSCB communication enable port
 
-/*--------------------------------*/
+/*---------FEB64-----------------------*/
 #elif defined(FEB64)
 #include <c8051F120.h>
 #define CPU_C8051F120
@@ -491,130 +491,23 @@ sbit RS485_ENABLE = P0 ^ 3; //MSCB communication enable port
 #define SST_ClientResponse1 (char) ((CPT1CN & 0x40) >> 6) //Comparator1 overflow bit
 
 #define LED_ON 0 //defines LED "ON" as forcing the specified pin to low
-// SMB  Port is fixed, XBR0 set in mscbmain
+// SMB
+#define MSCB_I2C_SDA P0 ^ 2
+#define MSCB_I2C_SCL P0 ^ 3
 
 // SPI
 #define MSCB_SPI_SCK  P2 ^ 3
 #define MSCB_SPI_MISO P2 ^ 1
 #define MSCB_SPI_MOSI P2 ^ 4
 
-sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
-
-/*--------------------------------*/
-#elif defined(TEMP36)
-#include <c8051F120.h>
-#define CPU_C8051F120
-#define LED_0 P2 ^ 7 
-#define LED_1 P2 ^ 6
-#define LED_ON 1
-sbit RS485_ENABLE = P0 ^ 2; //MSCB communication enable port
-
-// SST
-#define MORE_THAN_ONE_LINE	//2 SST Lines
-#define MSCB_SST1 P0^3 //SST1 line  SST_IO (Write/Push-Pull)
-#define MSCB_SST2 P0^4
-#define SST_ClientResponse1 (char) ((CPT0CN & 0x40) >> 6) //Comparator1 overflow bit
-#define SST_ClientResponse2 (char) ((CPT1CN & 0x40) >> 6)
-
-//SPI
-#define MSCB_SPI_SCK  P2 ^ 4
-#define MSCB_SPI_MISO P2 ^ 1
-#define MSCB_SPI_MOSI P2 ^ 5 
-
-#define HAVE_RTC
-
-//SHT
-#define SHT_DATA1 P1^2
-#define SHT_SCK1  P1^0
-#define SHT_DATA2 P1^6
-#define SHT_SCK2  P1^4
-
-//
-// LOADER
-//-----------------------------------------------------------------------
-#elif defined(LOADER)
-#include <c8051F120.h>
-#define CPU_C8051F120
-
-#if defined(LED_1) 
-#undef LED_1
-#endif
-   
-#define LED_ON 0 //defines LED "ON" as forcing the specified pin to low
-
-// SPI
-#ifdef L_TEMP36
-#define MSCB_SPI_SCK  P2 ^ 4
-#define MSCB_SPI_MISO P2 ^ 1
-#define MSCB_SPI_MOSI P2 ^ 5
-#define RAM_CHIP_SELECT    P2 ^ 0
-#define RAM_HOLD_DOWN      P2 ^ 3
-#define RAM_WRITE_PROTECT  P2 ^ 2
-#define LED_0 P2 ^ 7 
-sbit RS485_ENABLE = P0 ^ 2; //MSCB communication enable port
-
-//
-// FEB64 Loader
-//-----------------------------------------------------------------------
-#elif defined(L_FEB64)
-//
-// P3.7:RAMCSn   .6:CSn6      .5:CSn4     .4:SPARE5  | .3:SPARE4  .2:REG_EN   .1:CSn3    .0:CSn2 
-// P2.7:SPARE1   .6:CSn7      .5:CSn6     .4:SPIMOSI | .3:SPISCK  .2:RAMHLDn  .1:SPIMISO .0:RAMWPn 
-// P1.7:ASUMSync .6:ASUMTestn .5:ASUMPWDn .4:ASUMCSn | .3:ResetN  .2:SPARE2   .1:SPARE3  .0:SST_DRV 
-// P0.7:CSn1     .6:CSn0      .5:485TXEN  .4:QPUMPCLK| .3:SMBCLK  .2:SMBDAT   .1:Rx      .0:Tx 
-
-// SMB  Port is fixed, XBR0 set in mscbmain
-
-// SPI
-#define MSCB_SPI_SCK  P2 ^ 3
-#define MSCB_SPI_MISO P2 ^ 1
-#define MSCB_SPI_MOSI P2 ^ 4
 // EEPROM
 #define RAM_CHIP_SELECT    P3 ^ 7
 #define RAM_HOLD_DOWN      P2 ^ 2
 #define RAM_WRITE_PROTECT  P2 ^ 0
-// RS485
-#define LED_0 P2 ^ 7 
+
 sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 
-//
-// LPB Loader
-//-----------------------------------------------------------------------
-#elif defined(L_LPB)
-//
-// P3.7:A7       .6:A6        .5:A5       .4:A4      | .3:A3      .2:A2       .1:A1      .0:A0 
-// P2.7:+1.8En   .6:+3.3En    .5:+5En     .4:SPIMOSI | .3:SPISCK  .2:RAMHLDn  .1:SPIMISO .0:RAMWP
-// P1.7:NC       .6:+6ddFlag  .5:R/HClock .4:R/HData | .3:+6ddEN  .2:RAMCS    .1:D2ASync .0:SST_DRV 
-// P0.7:NC       .6:NC        .5:485TXEN  .4:NC      | .3:NC      .2:NC       .1:Rx      .0:Tx 
-
-//The pins that are used for Threshold voltages for comparator 0 and comparator 1
-#define MSCB_SST1 P1 ^ 0 //SST1 line  SST_IO (Write/Push-Pull)
-#define SST_ClientResponse1 (char) ((CPT1CN & 0x40) >> 6) //Comparator1 overflow bit
-
-#define MSCB_SPI_SCK  P2 ^ 3
-#define MSCB_SPI_MISO P2 ^ 1
-#define MSCB_SPI_MOSI P2 ^ 4
-#define RAM_CHIP_SELECT    P1 ^ 2
-#define RAM_HOLD_DOWN      P2 ^ 2
-#define RAM_WRITE_PROTECT  P2 ^ 0
-#define LED_0 P0 ^ 7 
-sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
-
-//
-// CMB Loader
-//-----------------------------------------------------------------------
-#elif defined(L_CMB)
-#define MSCB_SPI_SCK  P2 ^ 3
-#define MSCB_SPI_MISO P2 ^ 1
-#define MSCB_SPI_MOSI P2 ^ 4
-#define RAM_CHIP_SELECT    P3 ^ 7
-#define RAM_HOLD_DOWN      P2 ^ 2
-#define RAM_WRITE_PROTECT  P2 ^ 0
-#define LED_0 P2 ^ 7 
-sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
-#endif
-
-/*--------------------------------*/
+/*----------CMB----------------------*/
 #elif defined(CMB)
 #include <c8051F120.h>
 #define CPU_C8051F120
@@ -631,9 +524,19 @@ sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 #define MSCB_SST1 P1 ^ 0 //SST1 line  SST_IO (Write/Push-Pull)
 #define SST_ClientResponse1 (char) ((CPT1CN & 0x40) >> 6) //Comparator1 overflow bit
 
+// SPI
+#define MSCB_SPI_SCK  P1 ^ 2
+#define MSCB_SPI_MISO P1 ^ 6
+#define MSCB_SPI_MOSI P1 ^ 1
+
+//EEPROM
+#define RAM_CHIP_SELECT    P1 ^ 7
+#define RAM_HOLD_DOWN      P1 ^ 3
+#define RAM_WRITE_PROTECT  P1 ^ 5
+
 sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 
-/*--------------------------------*/
+/*---------LPB-----------------------*/
 #elif defined(LPB)
 #include <c8051F120.h>
 #define CPU_C8051F120
@@ -659,7 +562,145 @@ sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
 #define SHT_DATA1 P1^4
 #define SHT_SCK1  P1^5
 
+// EEPROM
+#define RAM_CHIP_SELECT    P1 ^ 2
+#define RAM_HOLD_DOWN      P2 ^ 2
+#define RAM_WRITE_PROTECT  P2 ^ 0
+
 sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
+
+/*---------TEMP36-----------------------*/
+#elif defined(TEMP36)
+#include <c8051F120.h>
+#define CPU_C8051F120
+#define LED_0 P2 ^ 7 
+#define LED_1 P2 ^ 6
+#define LED_ON 1
+sbit RS485_ENABLE = P0 ^ 2; //MSCB communication enable port
+
+// SST
+#define MORE_THAN_ONE_LINE	//2 SST Lines
+#define MSCB_SST1 P0^3 //SST1 line  SST_IO (Write/Push-Pull)
+#define MSCB_SST2 P0^4
+#define SST_ClientResponse1 (char) ((CPT0CN & 0x40) >> 6) //Comparator1 overflow bit
+#define SST_ClientResponse2 (char) ((CPT1CN & 0x40) >> 6)
+
+// SPI
+#define MSCB_SPI_SCK  P2 ^ 4
+#define MSCB_SPI_MISO P2 ^ 1
+#define MSCB_SPI_MOSI P2 ^ 5 
+
+// EEPROM
+//#define RAM_CHIP_SELECT    P2 ^ 0
+//#define RAM_HOLD_DOWN      P2 ^ 3
+//#define RAM_WRITE_PROTECT  P2 ^ 2
+
+// SHT
+#define SHT_DATA1 P1^2
+#define SHT_SCK1  P1^0
+#define SHT_DATA2 P1^6
+#define SHT_SCK2  P1^4
+
+#define HAVE_RTC
+//
+/*---------LOADER-----------------------*/
+#elif defined(LOADER)
+#include <c8051F120.h>
+#define CPU_C8051F120
+
+#if defined(LED_1) 
+#undef LED_1
+#endif
+// sbit DEBUG_PIN = P3 ^ 3;
+   
+#define LED_ON 0 //defines LED "ON" as forcing the specified pin to low
+
+//
+// FEB64 Loader
+//-----------------------------------------------------------------------
+#ifdef L_FEB64
+//
+// P3.7:RAMCSn   .6:CSn6      .5:CSn4     .4:SPARE5  | .3:SPARE4  .2:REG_EN   .1:CSn3    .0:CSn2 
+// P2.7:SPARE1   .6:CSn7      .5:CSn6     .4:SPIMOSI | .3:SPISCK  .2:RAMHLDn  .1:SPIMISO .0:RAMWPn 
+// P1.7:ASUMSync .6:ASUMTestn .5:ASUMPWDn .4:ASUMCSn | .3:ResetN  .2:SPARE2   .1:SPARE3  .0:SST_DRV 
+// P0.7:CSn1     .6:CSn0      .5:485TXEN  .4:QPUMPCLK| .3:SMBCLK  .2:SMBDAT   .1:Rx      .0:Tx 
+
+// SMB  Port is fixed, XBR0 set in mscbmain
+
+// SPI
+#define MSCB_SPI_SCK  P2 ^ 3
+#define MSCB_SPI_MISO P2 ^ 1
+#define MSCB_SPI_MOSI P2 ^ 4
+
+// EEPROM
+#define RAM_CHIP_SELECT    P3 ^ 7
+#define RAM_HOLD_DOWN      P2 ^ 2
+#define RAM_WRITE_PROTECT  P2 ^ 0
+// RS485
+#define LED_0 P2 ^ 7 
+sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
+
+//
+// CMB Loader
+//-----------------------------------------------------------------------
+#elif defined(L_CMB)
+// SPI
+#define MSCB_SPI_SCK  P1 ^ 2
+#define MSCB_SPI_MISO P1 ^ 6
+#define MSCB_SPI_MOSI P1 ^ 1
+
+//EEPROM
+#define RAM_CHIP_SELECT    P1 ^ 7
+#define RAM_HOLD_DOWN      P1 ^ 3
+#define RAM_WRITE_PROTECT  P1 ^ 5
+
+#define LED_0 P2 ^ 2 
+sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
+
+//
+// LPB Loader
+//-----------------------------------------------------------------------
+#elif defined(L_LPB)
+//
+// P3.7:A7       .6:A6        .5:A5       .4:A4      | .3:A3      .2:A2       .1:A1      .0:A0 
+// P2.7:+1.8En   .6:+3.3En    .5:+5En     .4:SPIMOSI | .3:SPISCK  .2:RAMHLDn  .1:SPIMISO .0:RAMWP
+// P1.7:NC       .6:+6ddFlag  .5:R/HClock .4:R/HData | .3:+6ddEN  .2:RAMCS    .1:D2ASync .0:SST_DRV 
+// P0.7:NC       .6:NC        .5:485TXEN  .4:NC      | .3:NC      .2:NC       .1:Rx      .0:Tx 
+
+//The pins that are used for Threshold voltages for comparator 0 and comparator 1
+#define MSCB_SST1 P1 ^ 0 //SST1 line  SST_IO (Write/Push-Pull)
+#define SST_ClientResponse1 (char) ((CPT1CN & 0x40) >> 6) //Comparator1 overflow bit
+
+// SPI
+#define MSCB_SPI_SCK  P2 ^ 3
+#define MSCB_SPI_MISO P2 ^ 1
+#define MSCB_SPI_MOSI P2 ^ 4
+
+//EEPROM
+#define RAM_CHIP_SELECT    P1 ^ 2
+#define RAM_HOLD_DOWN      P2 ^ 2
+#define RAM_WRITE_PROTECT  P2 ^ 0
+#define LED_0 P0 ^ 7 
+sbit RS485_ENABLE = P0 ^ 5; //MSCB communication enable port
+
+//
+// TEMP36 Loader
+//-----------------------------------------------------------------------
+#elif defined(L_TEMP36)
+// SPI
+#define MSCB_SPI_SCK  P2 ^ 4
+#define MSCB_SPI_MISO P2 ^ 1
+#define MSCB_SPI_MOSI P2 ^ 5
+
+//EEPROM
+#define RAM_CHIP_SELECT    P2 ^ 0
+#define RAM_HOLD_DOWN      P2 ^ 3
+#define RAM_WRITE_PROTECT  P2 ^ 2
+#define LED_0 P2 ^ 7 
+sbit RS485_ENABLE = P0 ^ 2; //MSCB communication enable port
+
+#endif  // LOADER 
+
 /*--------------------------------*/
 #elif defined(GPIB_TEST)
 #include <c8051F020.h>
