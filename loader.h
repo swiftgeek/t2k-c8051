1251fb2269fe0cd66993b2105d0176c342649e00
midas <midas@6c71da66-6f2f-0410-bb3c-c2389e3dc2a8>
2009-12-12 15:10:03 +0000

    change LPB: Vss from 0.2A to 0.3A
    git-svn-id: https://ladd00.triumf.ca/svn/fgddaq/trunk/slowcontrol/c8051@1066 6c71da66-6f2f-0410-bb3c-c2389e3dc2a8

--- loader.h
+++ loader.h
@@ -222,7 +222,7 @@ struct EEPAGE xdata eepage={
    , 0.0, 3.5, 0.1, 5.5, 0.0, 0.0, 0.0,  0.0
 // 0x12 - uVIlimit 
 //   D2A  Vss  Iss  Vdd  Idd  I5   I3.3  I1.8
-   , 6.0, 5.5, 0.2, 6.5, 1.8, 0.1, 0.5,  0.2  
+   , 6.0, 5.5, 0.3, 6.5, 1.8, 0.1, 0.5,  0.2  
 // 0x1a - LuC Temperature,  HuC Temperature
    , 10., 50.
 // 0x1c - LSST Temperature,  HSST Temperature
